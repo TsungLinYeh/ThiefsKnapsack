71c399c3ae02674a6198eac8804b8f30aa48f8d8
Jeffrey Yeh <nightcrow33@gmail.com>
2021-04-14 12:14:45 -0700

    add prompt

---
+++ answer.js
@@ -0,0 +1,12 @@
+/**
+ * to get an answer, each item should have two properties weight and worth.
+ * I: pack hold X, and array of object for all items
+ * O: maxmum worth of the pack can hold
+ * C:
+ * E:
+ * @param {Number} hold - lbs pack can hold
+ * @param {[Object]} items - array of objects each obj should have weight and worth properties
+ */
+const maxWorth(hold, items) => {
+
+}
